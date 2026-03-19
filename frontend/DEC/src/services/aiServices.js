7f6f7a9c10c90e5d17ed2554ec1603fef67685b3
jhon170920 <j276scc@gmail.com>
2026-03-19 16:38:37 -0500

    se arreglo la confianza en los resultados en que trabaja la IA

--- frontend/DEC/src/services/aiServices.js
+++ frontend/DEC/src/services/aiServices.js
@@ -96,7 +96,7 @@ export const processPrediction = (predictionData) => {
     3: "Araña roja"
   };
 
-  const UMBRAL = 0.40; // Confianza mínima del 40%
+  const UMBRAL = 0.70; // Confianza mínima del 40%
   let mejorConf = 0;
   let mejorClase = -1;
 
