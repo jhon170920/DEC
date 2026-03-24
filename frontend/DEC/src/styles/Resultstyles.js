e9b222124b7635aecd1ee388731893f911d70928
Juancho0-web <juanchoap06@gmail.com>
2026-03-24 17:05:49 -0500

    subir

---
+++ frontend/DEC/src/styles/Resultstyles.js
@@ -0,0 +1,10 @@
+import { StyleSheet } from "react-native";
+import { Colors } from "../constants/colors";
+
+export const ResultStyles = StyleSheet.create({
+    container:{
+        flex: 1,
+        backgroundColor: Colors.bg,
+
+    },
+})
