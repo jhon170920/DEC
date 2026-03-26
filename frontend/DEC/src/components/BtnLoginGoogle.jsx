9729317aa0b4c4de95cbf3566a39f2be02048411
joantttgo <107sebastiangomez@gmail.com>
2026-03-26 16:33:18 -0500

    Boton de Google terminado, creo

--- frontend/DEC/src/components/BtnLoginGoogle.jsx
+++ frontend/DEC/src/components/BtnLoginGoogle.jsx
@@ -1,7 +1,7 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useContext } from "react";
 import { TouchableOpacity, Text, StyleSheet } from "react-native";
 import { CommonActions, useNavigation } from "@react-navigation/native";
-import { setUserToken } from '../context/AuthContext'
+import { AuthContext } from '../context/AuthContext.js';
 
 import * as Google from 'expo-auth-session/providers/google'
 import * as AuthSession from 'expo-auth-session';
@@ -11,18 +11,21 @@ import axios from 'axios';
 WebBrowser.maybeCompleteAuthSession();
 
 export default function BtnloginGoogle() {
+    const { setUserToken } = useContext(AuthContext);
+    
     const navigation = useNavigation();
     const [loading, setLoading] = useState(false)
 
     const [request, response, promptAsync] = Google.useAuthRequest({
-        androidClientId: '487264865082-40i1lt5m0cum9s0m5i96v4menqnvgtgc.apps.googleusercontent.com',
+        androidClientId: process.env.EXPO_PUBLIC_ANDROID_CLIENT_ID,
     })
 
     useEffect(() => {
         if (response) {
             if (response.type === 'success') {
-                sendTokenToServer(response.authentication?.idToken || '')
+                console.log(response)
                 console.log('ttoken: ', response.authentication?.idToken)
+                sendTokenToServer(response.authentication?.idToken || '')
             } else {
                 console.log("error en l autenticacion: ", response)
             }
@@ -38,6 +41,9 @@ export default function BtnloginGoogle() {
             const response = await axios.post('http://10.4.1.148:8089/api/users/auth/google', {
                 token: token
             })
+            console.log(response.data.token);
+            console.log(response.data.message);
+            setUserToken(response.data.token)
         } catch (error) {
             console.log("Error No se pudo conectar con el servidor DEC", error.message);
         } finally {
