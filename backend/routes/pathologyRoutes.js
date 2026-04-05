bd1d228041ed244b5b3d34e64d6c40f2b4ecdf76
jhon170920 <j276scc@gmail.com>
2026-04-04 19:26:06 -0500

    comentario

--- backend/routes/pathologyRoutes.js
+++ backend/routes/pathologyRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { getAllPathologies } from '../controllers/pathologyController.js';
-import { verifyToken } from '../middlewares/auth.js'; // Asumo que tienes un middleware para proteger rutas
+import { verifyToken } from '../middlewares/auth.js'; // middleware para proteger rutas
 
 const router = express.Router();
 
