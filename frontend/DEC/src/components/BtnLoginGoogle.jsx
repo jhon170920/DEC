17f7d9e1e34b32265e0e61e852af40c0880fb73d
“M-DelMar” <mariadelmarchavezcuellar@gmail.com>
2026-04-10 17:05:27 -0500

    Login social (botones)

--- frontend/DEC/src/components/BtnLoginGoogle.jsx
+++ frontend/DEC/src/components/BtnLoginGoogle.jsx
@@ -1,88 +1,140 @@
 import { useEffect, useState, useContext } from "react";
-import { TouchableOpacity, Text, Image, StyleSheet } from "react-native";
-import { CommonActions, useNavigation } from "@react-navigation/native";
+import { TouchableOpacity, Text, Image, StyleSheet, Alert } from "react-native";
 import { AuthContext } from '../context/AuthContext.js';
 import { Colors } from '../constants/colors.js'
 import { useResponsiveLayout } from '../hooks/useResponsiveLayout';
 
-import * as Google from 'expo-auth-session/providers/google'
-import * as AuthSession from 'expo-auth-session';
-import * as WebBrowser from 'expo-web-browser';
 import axios from 'axios';
+import * as SecureStore from 'expo-secure-store';
+
+// LOGIN GOOGLE PLAY SERVICES (ANDROID PROMEDIO)
+import { GoogleSignin, statusCodes } from '@react-native-google-signin/google-signin';
+// LLOGIN SI NO TIENE GOOGLE PLAY SERVICES (MOTOROLA MODERNO, IOS, ROOM PERSONALIZADO)
+import { makeRedirectUri } from 'expo-auth-session';
+import * as Google from 'expo-auth-session/providers/google';
+import * as WebBrowser from 'expo-web-browser';
 
 WebBrowser.maybeCompleteAuthSession();
 
+GoogleSignin.configure({
+    webClientId: process.env.EXPO_PUBLIC_GOOGLE_WEB_CLIENT_ID, // SE USA EL ID DEL CLIENTE DE WEB PARA EL BACKEND
+    offlineAccess: true, // Si necesitas que el backend pida tokens nuevos
+});
+
 export default function BtnloginGoogle() {
     // ----RESPONSIVE LAYOUT
-    const {
-        sp,
-        hPad,
-        logoRingS,
-        logoImgS,
-        headlineS,
-        sublineS,
-        fieldH,
-        btnH,
-        ghostH,
-        socialH,
-        iconS
-    } = useResponsiveLayout();
+    const { iconS } = useResponsiveLayout();
     // traemos la funcion que actualiza el estado del token
     const { setUserToken } = useContext(AuthContext);
-    // navigation jajja
-    const navigation = useNavigation();
     // para el disabled del login y seguridad
-    const [loading, setLoading] = useState(false)
-    // funcion de google, le enviamos el id del cliente Android
+    const [loading, setLoading] = useState(false);
+
     const [request, response, promptAsync] = Google.useAuthRequest({
-        androidClientId: process.env.EXPO_PUBLIC_GOOGLE_CLIENT_ID,
-    })
+        androidClientId: process.env.EXPO_PUBLIC_GOOGLE_APP_CLIENT_ID,
+        webClientId: process.env.EXPO_PUBLIC_GOOGLE_WEB_CLIENT_ID,
+        responseType: 'code', 
+    
+        // CAMBIO 2: Asegúrate de que el Proxy esté activo
+        useProxy: true,
+        
+        scopes: ['profile', 'email'],
+        redirectUri: makeRedirectUri({
+        useProxy: true,
+        }),
+    });
 
-    // esto se realiza cuando se uno se loguea CORRECTAMENTE con google
     useEffect(() => {
-        if (response) {
-            if (response.type === 'success') {
-                // mandamos el token a nuestra funcion para verificar en el backend si es de google
-                sendTokenToServer(response.authentication?.idToken || '')
-            } else {
-                //para el debug, no sé si quitarlo o poner otra cosa o no sé
-                console.log("error en l autenticacion: ", response)
-            }
+        if (response?.type === 'success') {
+          const { id_token } = response.params;
+          sendTokenToServer(id_token);
         }
-    }, [response])
+    }, [response]);
 
+    // Enviar al backend y validar el token de google
     const sendTokenToServer = async (googleToken) => {
-        // cuando se loguea correctamente
-        setLoading(true)
+        setLoading(true);
         try {
-            // mandamos el googleToken para el backend
+            // mandamos el googleToken para el backend y validarlo
             const response = await axios.post('http://10.4.1.148:8089/api/users/auth/google',
                 {},
-                {
-                    headers: {
-                        'Authorization': `Bearer ${googleToken}`
-                    }
-                });
-            const token = response.data.token
-            if (token) {
-                setUserToken(token)
-                console.log("Sesion iniciada con éxito")
+                { headers: { 'Authorization': `Bearer ${googleToken}` }, timeout: 10000 }
+            );
+            // traemos nuestro propio token de la session del usuario
+            const sessionToken = response.data.token
+            if (sessionToken) {
+                // guardamos el token tanto en el secure store como en nuestro estado del token
+                await SecureStore.setItemAsync('userToken', response.data.token);
+                await setUserToken(sessionToken)
             }
         } catch (error) {
-            console.log("Error No se pudo conectar con el servidor DEC", error.message);
+            if (!error.response) {
+                // Error de red (el servidor está caído o no hay internet)
+                Alert.alert("Error de conexión", "No se pudo conectar con el servidor DEC. Verifica tu internet.");
+            } else if (error.response.status === 401) {
+                Alert.alert("Sesión inválida", "La autenticación con Google falló o expiró.");
+            } else {
+                Alert.alert("Error", "Ocurrió un problema al iniciar sesión. Inténtalo de nuevo.");
+            }
         } finally {
             setLoading(false);
         }
     }
+    // funcion del boton de google
+    const handleGoogleLogin = async () => {
+        setLoading(true)
+        try {
+            await promptAsync();
+        } catch (error) {
+            // SI NO TIENE PLAY SERVICES
+            if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+                console.log("Play Services no disponibles, activando navegador...");
+            }
+            // Validaciones silenciosas (donde no necesitas alarmar al usuario)
+            if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+                return; // El usuario simplemente cerró la ventana
+            }
 
+            if (error.code === statusCodes.IN_PROGRESS) {
+                return; // Ya se está intentando loguear
+            }
+
+        } finally {
+            setLoading(false)
+        }
+    }
+
+
+    // ver si ya tiene sesion con google para que no vuelva a iniciar sesion
+    useEffect(() => {
+        const checkCurrentSessionGoogle = async () => {
+            try {
+                // buscamos token local
+                const localToken = await SecureStore.getItemAsync('userToken');
+                if (localToken) {
+                    // Si existe el token de nuestra DB, lo cargamos al estado global
+                    await setUserToken(localToken);
+                    return; // Ya está logueado en nuestra App, no molestamos a Google.
+                }
+                // Revisar si el usuario ya inició sesión previamente en este dispositivo
+                const hasGoogleSession = await GoogleSignin.isSignedIn();
+                if (hasGoogleSession) {
+                    const userInfo = await GoogleSignin.signInSilently();
+                    const idToken = userInfo?.data?.idToken;
+                    // Mandamos el token al servidor para obtener el JWT de nuestra DB
+                    if (idToken) {
+                        await sendTokenToServer(idToken);
+                    }
+                    console.log("sesion de google detectada");
+                }
+            } catch (error) {
+                console.log("No hay sesión previa activa con Google");
+            }
+        };
+        checkCurrentSessionGoogle();
+    }, []);
     return (
         <TouchableOpacity style={styles.btn}
-            onPress={() => {
-                promptAsync().catch((e) => {
-                    console.error("Error al iniciar sesion:", e);
-                })
-                console.log("Iniciando propmt......")
-            }}
+            onPress={handleGoogleLogin}
             disabled={loading}
 
         >
